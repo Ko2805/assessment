cdd1a089c2f08a121428c86d5f5c4a6fc9c89ffb
Ko2805 <251zs00949@student.zen.ac.jp>
2026-08-05 22:51:14 +0900

    Update anime.js

--- anime.js
+++ anime.js
@@ -2,23 +2,31 @@ let heading = document.getElementById('heading');
 
 let degree = 0;
 
+// 最初の色
+heading.className = 'color1';
+
 function rotateHeading() {
   degree = degree + 6;
-  degree = degree % 360;
 
-  if (degree === 0) {
-    heading.setAttribute('class', 'color1');
-
-  } else if (degree === 90) {
-    heading.setAttribute('class', 'color2');
-
-  } else if (degree === 180) {
-    heading.setAttribute('class', 'color3');
-
-  } else if (degree === 270) {
-    heading.setAttribute('class', 'color4');
+  if (degree >= 360) {
+    degree = 0;
   }
 
+  // 回転角度によって色を変更
+  if (degree < 90) {
+    heading.className = 'color1';
+
+  } else if (degree < 180) {
+    heading.className = 'color2';
+
+  } else if (degree < 270) {
+    heading.className = 'color3';
+
+  } else {
+    heading.className = 'color4';
+  }
+
+  // X軸方向に回転
   heading.style.transform = 'rotateX(' + degree + 'deg)';
 }
 
