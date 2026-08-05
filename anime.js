1b5aae4407107c1fb556babec9cf6649b7c0b2e2
Ko2805 <251zs00949@student.zen.ac.jp>
2026-08-05 23:24:46 +0900

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  let heading = document.getElementById('heading');
+  let heading = document.getElementById('lucky');
 
   let degree = 0;
 
