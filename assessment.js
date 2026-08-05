49f18c679e98fc7d6c82b402d4478905f53c495f
Ko2805 <251zs00949@student.zen.ac.jp>
2026-08-05 19:01:12 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -4,12 +4,14 @@ const assessmentButton = document.getElementById('assessment');
 const resultDivision = document.getElementById('result-area');
 const tweetDivision = document.getElementById('tweet-area');
 
-assessmentButton.onclick = () => {
-  const userName = userNameInput.value;
-  if (userName.length === 0) {
-    // 名前が空の時は処理を終了する
-    return;
-  }
+assessmentButton.addEventListener(
+  'click',
+  function () {
+    const userName = userNameInput.value;
+     if (userName.length === 0) {
+       // 名前が空の時は処理を終了する
+       return;
+     }
 
   // 診断結果表示エリアの作成
   resultDivision.innerText = '';
@@ -51,17 +53,21 @@ assessmentButton.onclick = () => {
 
   tweetDivision.appendChild(anchor);
 
-  const script = document.createElement('script');
+ 
+    const script = document.createElement('script');
   script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
   tweetDivision.appendChild(script);
+  }  
 };
 
-userNameInput.onkeydown = event => {
-  if (event.key === 'Enter') {
-    assessmentButton.onclick();
+userNameInput.addEventListener(
+  'keydown',
+  (event) => {
+    if(event.code === 'Enter') {
+      assessmentButton.dispatchEvent(new Event('click'))
+    }
   }
-};
-
+)
 const answers = [
   '###userName###のいいところは声です。###userName###の特徴的な声は皆を惹きつけ、心に残ります。',
   '###userName###のいいところはまなざしです。###userName###に見つめられた人は、気になって仕方がないでしょう。',
@@ -79,6 +85,7 @@ const answers = [
   '###userName###のいいところは気配りです。###userName###の配慮が多くの人を救っています。',
   '###userName###のいいところはその全てです。ありのままの###userName###自身がいいところなのです。',
   '###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。'
+  '###userName###のいいところは優しさです。###userName###の優しい雰囲気や立ち振る舞いに多くの人が癒やされています。'
 ];
 
 /**
@@ -86,28 +93,87 @@ const answers = [
  * @param {string} userName ユーザの名前
  * @return {string} 診断結果
  */
-function assessment(userName) {
+ function assessment(userName) {
   // 全文字のコード番号を取得してそれを足し合わせる
   let sumOfCharCode = 0;
   for (let i = 0; i < userName.length; i++) {
     sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
   }
-
+ 
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
   const index = sumOfCharCode % answers.length;
   let result = answers[index];
-
+ 
   result = result.replaceAll('###userName###', userName);
   return result;
 }
 
-// テストコード
-console.assert(
-  assessment('太郎') ===
-    '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
-  '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
-);
-console.assert(
-  assessment('太郎') === assessment('太郎'),
-  '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
-);
+// テストを行う関数
+function test() {
+  console.log('診断結果の文章のテスト');
+
+  //太郎
+  console.log('太郎');
+  console.assert(
+    assessment('太郎') ===
+      '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+    '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+  );
+
+  //次郎
+  console.log('次郎');
+  console.assert(
+    assessment('次郎') ===
+      '次郎のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる次郎が皆から評価されています。',
+    '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+  );
+
+  //花子
+  console.log('花子');
+  console.assert(
+    assessment('花子') ===
+      '花子のいいところはまなざしです。花子に見つめられた人は、気になって仕方がないでしょう。',
+    '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+  );
+  
+  console.log('診断結果の文章のテスト終了');
+
+  console.log('同じ名前なら、同じ結果を出力することのテスト');
+
+  console.log('太郎');
+  console.assert(
+    assessment('太郎') === assessment('太郎'),
+    '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+  );
+
+  console.log('次郎');
+  console.assert(
+    assessment('次郎') === assessment('次郎'),
+    '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+  );
+
+  console.log('花子');
+  console.assert(
+    assessment('花子') === assessment('花子'),
+    '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+  );
+
+ console.log('同じ名前なら、同じ結果を出力することのテスト終了');
+}
+
+ test();
+
+ let heading = document.getElementById('heading');
+ heading.style.transform = 'rotateX(60deg)';
+  let degree = 0;
+ function rotateHeading() {
+   degree = degree + 6; 
+   degree = degree % 360;
+   if (degree === 90) {
+     heading.setAttribute('class', 'back');
+   } else if (degree === 270) {
+     heading.setAttribute('class', 'face');
+   }
+   heading.style.transform = 'rotateX(' + degree + 'deg)';
+ }
+ setInterval(rotateHeading, 20);
