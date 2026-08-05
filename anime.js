cbda21358df32b49ef8242fc644fedf85b5bc740
Ko2805 <251zs00949@student.zen.ac.jp>
2026-08-05 21:58:02 +0900

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,1 +1,10 @@
+let degree = 0;
+const colors = ["red", "yello", "green", "blue"];
+setInterval(() => {
+  degree += 90;
 
+  text.style.transform = `rotate(${degree}deg)`;
+
+  const colorIndex = (degree / 90) % 4;
+  text.style.color = colors[colorIndex];
+}, 1000);
