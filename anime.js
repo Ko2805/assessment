f756540f64da31dfc7ac2dd375a17c547e8771dc
Ko2805 <251zs00949@student.zen.ac.jp>
2026-08-05 23:28:27 +0900

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  let heading = document.getElementById('lucky');
+  let lucky = document.getElementById('lucky');
 
   let degree = 0;
 
-  function rotateHeading() {
+  function rotateLucky() {
 
     degree = degree + 6;
 
@@ -13,21 +13,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     if (degree < 90) {
-      heading.className = 'color1';
+      lucky.className = 'color1';
 
     } else if (degree < 180) {
-      heading.className = 'color2';
+      lucky.className = 'color2';
 
     } else if (degree < 270) {
-      heading.className = 'color3';
+      lucky.className = 'color3';
 
     } else {
-      heading.className = 'color4';
+      lucky.className = 'color4';
     }
 
-    heading.style.transform = 'rotateX(' + degree + 'deg)';
+    lucky.style.transform = 'rotateX(' + degree + 'deg)';
   }
 
-  setInterval(rotateHeading, 20);
+  setInterval(rotateLucky, 20);
 
 });
