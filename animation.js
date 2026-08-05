bef8d2748968fb97dfa8c4415a1f216f7d44d5f8
Ko2805 <251zs00949@student.zen.ac.jp>
2026-08-05 22:36:07 +0900

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,25 +1,14 @@
 let heading = document.getElementById('heading');
-
-let degree = 0;
-
-function rotateHeading() {
-  degree = degree + 6;
-  degree = degree % 360;
-
-  if (degree === 0) {
-    heading.setAttribute('class', 'face');
-
-  } else if (degree === 90) {
-    heading.setAttribute('class', 'back');
-
-  } else if (degree === 180) {
-    heading.setAttribute('class', 'color3');
-
-  } else if (degree === 270) {
-    heading.setAttribute('class', 'color4');
-  }
-
-  heading.style.transform = 'rotateX(' + degree + 'deg)';
-}
-
-setInterval(rotateHeading, 20);
+ heading.style.transform = 'rotateX(60deg)';
+  let degree = 0;
+ function rotateHeading() {
+   degree = degree + 6; 
+   degree = degree % 360;
+   if (degree === 90) {
+     heading.setAttribute('class', 'back');
+   } else if (degree === 270) {
+     heading.setAttribute('class', 'face');
+   }
+   heading.style.transform = 'rotateX(' + degree + 'deg)';
+ }
+ setInterval(rotateHeading, 20);
