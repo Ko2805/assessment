ca0c2ff416cbe498415cbdab016c0233a14e0a65
Ko2805 <251zs00949@student.zen.ac.jp>
2026-08-05 21:18:06 +0900

    Create animation.js

---
+++ animation.js
@@ -0,0 +1,14 @@
+let heading = document.getElementById('heading');
+ heading.style.transform = 'rotateX(60deg)';
+  let degree = 0;
+ function rotateHeading() {
+   degree = degree + 6; 
+   degree = degree % 360;
+   if (degree === 90) {
+     heading.setAttribute('class', 'back');
+   } else if (degree === 270) {
+     heading.setAttribute('class', 'face');
+   }
+   heading.style.transform = 'rotateX(' + degree + 'deg)';
+ }
+ setInterval(rotateHeading, 20);
